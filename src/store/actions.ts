05fecadcb6b74b7a1db16e8d0afe03d14572b65d
MagdalenaGie <magdalenaxgorska@gmail.com>
2021-12-14 17:38:52 +0100

    probably able to do some test

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,23 +1,23 @@
 import axios from './../axios';
 import { Dispatch } from "redux";
-import plotMockData2 from "./../components/plotting/mockResponse2.json";
-import plotMockData from "./../components/plotting/mockResponse.json";
-import statusMockData from './../components/controllers/mockResponse.json';
+// import plotMockData2 from "./../components/plotting/mockResponse2.json";
+// import plotMockData from "./../components/plotting/mockResponse.json";
+// import statusMockData from './../components/controllers/mockResponse.json';
 import { AuthActionTypes, DataActionTypes, GET_DATA_FAILURE, GET_DATA_SUCCESS, GET_STATUS_FAILURE, GET_STATUS_SUCCESS, SET_INTERVAL_ID, StatusActionTypes, USER_LOGIN_FAILURE, USER_LOGIN_SUCCESS, USER_LOGOUT } from './types';
 
 export const login = (username: string, password: string) => async (
 	dispatch: Dispatch<AuthActionTypes>
 ) => {
 	try {
-		//const res = await axios.post("/login", { username, password }); 
+		const res = await axios.post("/login", { username, password }); 
 		dispatch({
 			type: USER_LOGIN_SUCCESS,
 			payload: {
 				user: {
                     login: username,
                     password: password,
-                    //token: res.data.token
-                    token: "hellodarknessmyoldfriend",
+                    token: res.data.token
+                    //token: "hellodarknessmyoldfriend",
                 }
 			},
 		});
@@ -52,25 +52,25 @@ export const getData = (start: string, stop: string, token: string) => async (
 	dispatch: Dispatch<DataActionTypes>
 ) => {
 	try {
-		// const body = {
-		// 	start: start,
-		// 	stop: stop,
-		//   };
-		// const res = await axios.post("/history", body, {headers: { Authorization: `Bearer ${token}` }});
-		console.log("in getData -> before");
-		await sleep(4000);
-		console.log("in getData -> after");
+		const body = {
+			start: start,
+			stop: stop,
+		  };
+		const res = await axios.post("/history", body, {headers: { Authorization: `Bearer ${token}` }});
+		// console.log("in getData -> before");
+		// await sleep(4000);
+		// console.log("in getData -> after");
 
-		var res = {
-			data: plotMockData
-		}
+		// var res = {
+		// 	data: plotMockData
+		// }
 
-		if(token==="haslomaslo"){
-			res = {
-				data: plotMockData2
-			}
-			console.log("change")
-		}
+		// if(token==="haslomaslo"){
+		// 	res = {
+		// 		data: plotMockData2
+		// 	}
+		// 	console.log("change")
+		// }
 
 		dispatch({
 			type: GET_DATA_SUCCESS,
@@ -79,6 +79,7 @@ export const getData = (start: string, stop: string, token: string) => async (
 			},
 		});
 	} catch (err: unknown) {
+		console.log(err);
 		dispatch({
 			type: GET_DATA_FAILURE,
 			payload: {
@@ -92,11 +93,11 @@ export const getStatus = (token: string) => async (
 	dispatch: Dispatch<StatusActionTypes>
 ) => {
 	try {
-		//const res = await axios.get("/health_check", {headers: { Authorization: `Bearer ${token}` }});
-		console.log("in getStatus")
-		const res = {
-			data: statusMockData
-		}
+		const res = await axios.get("/health_check", {headers: { Authorization: `Bearer ${token}` }});
+		// console.log("in getStatus")
+		// const res = {
+		// 	data: statusMockData
+		// }
 		dispatch({
 			type: GET_STATUS_SUCCESS,
 			payload: {
@@ -104,6 +105,7 @@ export const getStatus = (token: string) => async (
 			},
 		});
 	} catch (err: unknown) {
+		console.log(err);
 		dispatch({
 			type: GET_STATUS_FAILURE,
 			payload: {
@@ -115,6 +117,6 @@ export const getStatus = (token: string) => async (
 
 
 //DELETE LATER 
-function sleep(ms: number) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-  }
+// function sleep(ms: number) {
+// 	return new Promise(resolve => setTimeout(resolve, ms));
+//   }
